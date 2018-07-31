262b701357da7a3eaff86a60510d11a842c44162
eugeneOwl <eugenevaleska1994@gmail.com>
2018-07-31 15:50:49 +0300

    Initial commit
    UserService & UserListComponent created.
    UserListComponent now shows the list of users
    getting it by JSON from service provided port.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserService } from './service/user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserListComponent } from './component/user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
